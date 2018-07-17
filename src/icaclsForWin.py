4ca32dcd70f4dc540ff8ed7bbb5b0ddcc626277e
ycf <17405114@life.hkbu.edu.hk>
2018-07-17 16:50:47 +0800

    for multiple users

--- src/icaclsForWin.py
+++ src/icaclsForWin.py
@@ -29,11 +29,13 @@ def checkPermission(path):
 		. NT AUTHORITY\\SYSTEM:(I)(OI)(CI)(F)\n,
 		BUILDIN\\Administrators:(I)(OI)(CI)(F)\n,
 		admin-PC\\admin:(I)(OI)(CI)(F)\n
+		\n
+		已顺利处理...
 	'''
 	# 每个用户都有一个权限, 此处，只为一个Administrators & 一个 admin
-	get_path_permission[path][file_info[0].split(':')[0]] = file_info[0].split(':')[1]
-	get_path_permission[path][file_info[1].split(':')[0]] = file_info[1].split(':')[1]
-	get_path_permission[path][file_info[2].split(':')[0]] = file_info[2].split(':')[1]
+	for user_index in range(len(file_info)-2):
+		get_path_permission[path][file_info[user_index].split(':')[0]] = file_info[user_index].split(':')[1]
+
 
 
 
